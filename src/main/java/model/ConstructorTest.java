97660ca2ba81c236924eaf2e51899cb719471b74
jeromecook <jeromecook@172.31.98.125>
2016-05-06 09:52:56 -0700

    adding model data

---
+++ src/main/java/model/ConstructorTest.java
@@ -0,0 +1,12 @@
+package model;
+
+public class ConstructorTest {
+
+	public ConstructorTest(){
+		System.out.println("insdie const test ");
+	}
+	
+	public String shoot(){
+		return "shoot method";
+	}
+}
